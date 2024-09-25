da8d79ee405715f6453df9b4a562b7b342d54fa2
daninasidi <daninasidi979@gmail.com>
2024-09-25 08:42:51 +0700

    Add files via upload

---
+++ 104_Moh Dani/temakampus/footer.php
@@ -0,0 +1,9 @@
+<footer> 
+    <p>&copy; 2024 Universitas Trunojoyo Madura. All rights reserved.</p> 
+    <p><a href="#">Tentang Kami</a> | <a href="#">087887017828</a> | <a href="#">Kebijakan 
+Privasi</a></p> 
+</footer> 
+ 
+<?php wp_footer(); ?> 
+</body> 
+</html>

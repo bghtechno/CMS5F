0e91559bb05e4e597fd875bed94699f15e4f658b
bghtechno <bungah.techno@gmail.com>
2024-09-25 08:20:37 +0700

    Delete 123-halimahkosong directory
    hapus folder contoh. beralih ke folder bernama CONTOH

--- 123-halimahkosong/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-<p>&copy; <?php echo date('Y'); ?> Nama Institusi. All rights reserved.</p>
-<p>Alamat: Jl. Contoh No. 123, Kota Contoh, Negara</p>
-<p>Email: info@institusi.com | Telepon: (021) 123-4567</p>
-</footer>
-<?php wp_footer(); ?>
-</body>
-</html>

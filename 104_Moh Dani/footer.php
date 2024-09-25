f085e64550de5961f3453f5de1a278cd444dab3d
daninasidi <daninasidi979@gmail.com>
2024-09-25 08:32:27 +0700

    Add files via upload

---
+++ 104_Moh Dani/footer.php
@@ -0,0 +1,8 @@
+<footer>
+<p>&copy; <?php echo date('Y'); ?> Moh Dani.</p>
+<p>Alamat: Ds. Manoan Kec. Kokop Kab. Bangkalan</p>
+<p>Email: daninasidi979.com | Telepon: 087887017828</p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
